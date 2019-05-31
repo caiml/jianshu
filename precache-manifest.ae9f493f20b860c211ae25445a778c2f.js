d58111b2c97d97a602210e449d6c06420e279a37
caiml <caimemory@gmail.com>
2019-05-31 14:53:29 +0800

    Updates

---
+++ precache-manifest.ae9f493f20b860c211ae25445a778c2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b29e0cdb2fc25f8a40097aa98256aef",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "f5a48ffded3b9d6cf42b",
+    "url": "/jianshu/static/css/main.b857d811.chunk.css"
+  },
+  {
+    "revision": "18f88aead3c33a75f1a1",
+    "url": "/jianshu/static/js/2.3eaf0c74.chunk.js"
+  },
+  {
+    "revision": "f5a48ffded3b9d6cf42b",
+    "url": "/jianshu/static/js/main.8436d9b0.chunk.js"
+  },
+  {
+    "revision": "74126ff601443a39a334",
+    "url": "/jianshu/static/js/runtime~main.362de7da.js"
+  },
+  {
+    "revision": "07302a75df624ca949e448967c69c2b8",
+    "url": "/jianshu/static/media/iconfont.07302a75.svg"
+  },
+  {
+    "revision": "0ead671743894b44a57d7012251c001a",
+    "url": "/jianshu/static/media/iconfont.0ead6717.eot"
+  },
+  {
+    "revision": "81613002205fb30f1d1e0d9fc5d6c66d",
+    "url": "/jianshu/static/media/iconfont.81613002.woff"
+  },
+  {
+    "revision": "d8f70f811b0cd95847cc3a13abc22a49",
+    "url": "/jianshu/static/media/iconfont.d8f70f81.ttf"
+  }
+]);
